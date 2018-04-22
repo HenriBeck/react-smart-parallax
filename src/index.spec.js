d210af22d1781e903913bbb9b416c60b41b711ed
Henri Beck <henribeck.dev@gmail.com>
2018-04-22 13:22:01 +0200

    Added a few more tests Updated nyc config

--- src/index.spec.js
+++ src/index.spec.js
@@ -25,3 +25,27 @@ test('should render correct dom elements', (t) => {
   t.deepEqual(wrapper.find('div.parallax--content').length, 1);
   t.deepEqual(wrapper.find('div.parallax--content').text(), 'Children');
 });
+
+test('should not compute the values and position the image when it\'s not loaded yet', (t) => {
+  const wrapper = mount(
+    <Parallax img="img">Children</Parallax>
+  );
+
+  wrapper
+    .find('EventListener')
+    .prop('onResize')();
+
+  t.pass();
+});
+
+test('should compute the values and the position when the image is loaded', (t) => {
+  const wrapper = mount(
+    <Parallax img="img">Children</Parallax>
+  );
+
+  wrapper
+    .find('img')
+    .simulate('load');
+
+  t.pass();
+});
